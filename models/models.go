3f2d918f3bc5ff61eb584aabb242adcf58d02a9c
dmitry-msk777 <dima-irk35@mail.ru>
2021-07-12 18:46:18 +0300

    new

--- models/models.go
+++ models/models.go
@@ -75,3 +75,90 @@ type AllJobs struct {
 	Priod      string
 	JSONString string
 }
+
+//TODO: Перенести в общий репозиторий
+type RemoteJob struct {
+	JobID        string `json:"ИдентификаторЗадания"`
+	RemoteBaseID string `json:"ИдентификаторУдаленнойБазы"`
+}
+
+//TODO: Перенести в общий репозиторий
+type QueryToBISimpleID struct {
+	JobID string `json:"ИдентификаторЗадания,JobID"`
+	Time  string
+}
+
+//TODO: Перенести в общий репозиторий
+type SettingsJobSliceQueryToBI struct {
+	JobID          string      `json:"ИдентификаторЗадания"`
+	SliceQueryToBI []QueryToBI `json:"Настройки"`
+}
+
+//TODO: Перенести в общий репозиторий
+type QueryToBI struct {
+	JobID                   string        `json:"ИдентификаторЗадания"`
+	Portions                int           `json:"Порции"`
+	Query                   []Query       `json:"Запросы"`
+	AddParam                AdditionParam `json:"ДополнительныеПараметрыJSON"`
+	AddParamJSNOString      string        `json:"JSONСтрокаДополнительныеПараметры"`
+	Connect                 Connect       `json:"ПараметрыПодключения"`
+	ConnectContur           ConnectContur `json:"ПараметрыПодключенияКонтура"`
+	ConnectBI1C             ConnectBI1C   `json:"ПараметрыПодключенияBI1C"`
+	ConnectConturJSNOString string        `json:"JSONСтрокаПараметрыПодключенияКонтура"`
+}
+
+type Query struct {
+	QueryText                            string `json:"Запрос"`
+	Base                                 string `json:"База"`
+	Area                                 string `json:"Области"`
+	ExchangeJobID                        string `json:"ИдентификаторЗапроса"`
+	PText                                string `json:"ПараметрыЗапроса"`
+	UsedCalculatedFieldsInQueryParametrs bool   `json:"ИспользуетсяВычисляемыеПоляВПараметрахЗапроса"`
+}
+
+type AdditionParam struct {
+	ZipAnswer  bool     `json:"СжиматьОтвет"`
+	HashAnswer bool     `json:"ХешироватьРезультат"`
+	Options    *Options `json:"НастройкиМоделиДанных"`
+	Connect    Connect  `json:"ПараметрыПодключенияHTTPОтвета"`
+}
+
+type Connect struct {
+	AddressServer   string `json:"АдресСервиса"`
+	PortServer      int    `json:"Порт"`
+	Resource        string `json:"Ресурс"`
+	SecureConnetion bool   `json:"ЗащищенноеСоединение"`
+	LoginConnetion  string `json:"Логин"`
+	Password        string `json:"Пароль"`
+	Headers         string `json:"Заголовки"`
+}
+
+type ConnectBI1C struct {
+	AddressServer   string `json:"АдресСервиса"`
+	PortServer      int    `json:"Порт"`
+	Resource        string `json:"Ресурс"`
+	SecureConnetion bool   `json:"ЗащищенноеСоединение"`
+	LoginConnetion  string `json:"Логин"`
+	Password        string `json:"Пароль"`
+	Headers         string `json:"Заголовки"`
+}
+
+type ConnectContur struct {
+	AddressServer   string `json:"АдресСервисаПриемника"`
+	PortServer      int    `json:"Порт"`
+	Resource        string `json:"Ресурс"`
+	SecureConnetion bool   `json:"ЗащищенноеСоединение"`
+	LoginConnetion  string `json:"Логин"`
+	Password        string `json:"Пароль"`
+	Headers         string `json:"Заголовки"`
+}
+
+type Options struct {
+	Description      string   `json:"НаименованиеЗадания"`
+	TableName        string   `json:"ИмяТаблицы"`
+	HardRemoval      bool     `json:"ПолноеУдаление"`
+	SelectionFields  []string `json:"ПоляОтбора"`
+	ComparionFields  []string `json:"ПоляСравнения"`
+	CompareAllFields bool     `json:"СравниватьПоВсемПолям"`
+	CompressBody     bool     `json:"СжиматьОтвет"`
+}
