ca6dca634984a15fd07a38a1e3c7f9e6fe150af2
dmitry-msk777 <dima-irk35@mail.ru>
2022-02-13 04:58:10 +0300

    new

--- utility/all.go
+++ utility/all.go
@@ -8,7 +8,7 @@ func GetAreasByStasus(DB *sql.DB, JobID, Stasus string) ([]string, error) {
 	argsquery = append(argsquery, JobID)
 	argsquery = append(argsquery, Stasus)
 
-	queryAllColumns := `select
+	QueryText := `select
 		area
 	from
 		public.exchange_jobs
@@ -16,7 +16,37 @@ func GetAreasByStasus(DB *sql.DB, JobID, Stasus string) ([]string, error) {
 		job_id = $1
 		and "event" = $2;`
 
-	rows, err := DB.Query(queryAllColumns, argsquery...)
+	rows, err := DB.Query(QueryText, argsquery...)
+	if err != nil {
+		return nil, err
+	}
+
+	var AreasForReturn []string
+	for rows.Next() {
+		var area string
+		err = rows.Scan(&area)
+		if err != nil {
+			return nil, err
+		}
+		AreasForReturn = append(AreasForReturn, area)
+	}
+
+	defer rows.Close()
+
+	return AreasForReturn, nil
+
+}
+
+func GetAreasNotEqualToStatus(DB *sql.DB, JobID, Stasus string) ([]string, error) {
+
+	var argsquery []interface{}
+	argsquery = append(argsquery, JobID)
+	argsquery = append(argsquery, Stasus)
+
+	QueryText := `SELECT area
+	FROM public.exchange_jobs where job_id = $1 and "event" <> $2;`
+
+	rows, err := DB.Query(QueryText, argsquery...)
 	if err != nil {
 		return nil, err
 	}
