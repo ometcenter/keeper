e16d8b4d97eaf102f7e8b40e7c68ddb1da5d54c6
dmitry-msk777 <dima-irk35@mail.ru>
2022-06-06 16:23:02 +0300

    fix

--- web/lkr.go
+++ web/lkr.go
@@ -346,7 +346,7 @@ func V1HolidayStatGeneral(WorkerID string, UseYearFilter bool, yearFilterFrom, y
 				re := regexp.MustCompile(`\d{2}.\d{2}.\d{4}`)
 				date_from_subjectArray := re.FindAllString(year, -1)
 				if len(date_from_subjectArray) == 0 {
-					log.Impl.Errorf("Ошибка парсинга даты для метода %s collaborator_id %s :", "V1HolidayStatGeneral", WorkerID)
+					log.Impl.Errorf("Ошибка парсинга даты для метода: %s collaborator_id: %s\n", "V1HolidayStatGeneral", WorkerID)
 					continue
 				}
 				//fmt.Printf("%q\n", date_from_subjectArray)
@@ -505,7 +505,7 @@ func V1BudgetStatGeneral(WorkerID string, UseYearFilter bool, yearFilter string,
 		re := regexp.MustCompile(`\d{2}.\d{2}.\d{4}`)
 		date_from_subjectArray := re.FindAllString(r.DateRegistration, -1)
 		if len(date_from_subjectArray) == 0 {
-			log.Impl.Errorf("Ошибка парсинга даты для метода %s collaborator_id %s :", "V1BudgetStatGeneral", WorkerID)
+			log.Impl.Errorf("Ошибка парсинга даты для метода: %s collaborator_id: %s\n", "V1BudgetStatGeneral", WorkerID)
 			continue
 		}
 
@@ -1084,7 +1084,7 @@ order by 2`
 
 		DateStartArray := re.FindAllString(r.DateStart, -1)
 		if len(DateStartArray) == 0 {
-			log.Impl.Errorf("Ошибка парсинга даты для метода %s collaborator_id %s :", "V1HolidayStatForColleaguesGeneral", r.CollaboratorId)
+			log.Impl.Errorf("Ошибка парсинга даты для метода: %s collaborator_id: %s\n", "V1HolidayStatForColleaguesGeneral", r.CollaboratorId)
 			continue
 		}
 		DateStart, err := time.Parse("02.01.2006", DateStartArray[0])
@@ -1094,7 +1094,7 @@ order by 2`
 
 		DateEndArray := re.FindAllString(r.DateEnd, -1)
 		if len(DateEndArray) == 0 {
-			log.Impl.Errorf("Ошибка парсинга даты для метода %s collaborator_id %s :", "V1HolidayStatForColleaguesGeneral", r.CollaboratorId)
+			log.Impl.Errorf("Ошибка парсинга даты для метода: %s collaborator_id: %s\n", "V1HolidayStatForColleaguesGeneral", r.CollaboratorId)
 			continue
 		}
 		DateEnd, err := time.Parse("02.01.2006", DateEndArray[0])
@@ -1467,7 +1467,7 @@ func V1AverageSalaryGeneral(WorkerID string, UseYearFilter bool, yearFilter stri
 		re := regexp.MustCompile(`\d{2}.\d{2}.\d{4}`)
 		date_from_subjectArray := re.FindAllString(r.DateRegistration, -1)
 		if len(date_from_subjectArray) == 0 {
-			log.Impl.Errorf("Ошибка парсинга даты для метода %s collaborator_id %s :", "V1AverageSalaryGeneral", WorkerID)
+			log.Impl.Errorf("Ошибка парсинга даты для метода: %s collaborator_id: %s\n", "V1AverageSalaryGeneral", WorkerID)
 			continue
 		}
 		//fmt.Printf("%q\n", date_from_subjectArray)
