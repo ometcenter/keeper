ad4fa0dfcd5d33c9a45f5246f1cc5bbbd97c4499
dmitry-msk777 <dima-irk35@mail.ru>
2022-06-21 01:10:06 +0300

    new

--- secure/auth.go
+++ secure/auth.go
@@ -19,50 +19,50 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// type MyCustomClaims struct {
-// 	UserId uint `json:"userid"`
-// 	jwt.StandardClaims
-// }
+type MyCustomClaimsLight struct {
+	UserId uint `json:"userid"`
+	jwt.StandardClaims
+}
 
-// func GetMiddleWareForGin() gin.HandlerFunc {
-// 	return func(c *gin.Context) {
+func GetMiddleWareLight() gin.HandlerFunc {
+	return func(c *gin.Context) {
 
-// 		// requestPath := c.Request.URL.Path          //current request path
+		// requestPath := c.Request.URL.Path          //current request path
 
-// 		tokenHeader := c.GetHeader("TokenBearer")
+		tokenHeader := c.GetHeader("TokenBearer")
 
-// 		if tokenHeader == "" {
-// 			log.Impl.Error("Отсутствует токен авторизации")
-// 			c.AbortWithStatusJSON(403, gin.H{"error": "Отсутствует токен авторизации"})
-// 			return
-// 		}
+		if tokenHeader == "" {
+			log.Impl.Error("Отсутствует токен авторизации")
+			c.AbortWithStatusJSON(403, gin.H{"error": "Отсутствует токен авторизации"})
+			return
+		}
 
-// 		//claims := jwt.MapClaims{}  // Если хотим просто выгрузить в map и перебрать открытые данные
-// 		//claims := jwt.StandardClaims{}
-// 		claims := &MyCustomClaims{}
-// 		token, err := jwt.ParseWithClaims(tokenHeader, claims, func(token *jwt.Token) (interface{}, error) {
-// 			return []byte(config.Conf.SecretKeyJWT), nil
-// 		})
+		//claims := jwt.MapClaims{}  // Если хотим просто выгрузить в map и перебрать открытые данные
+		//claims := jwt.StandardClaims{}
+		claims := &MyCustomClaimsLight{}
+		token, err := jwt.ParseWithClaims(tokenHeader, claims, func(token *jwt.Token) (interface{}, error) {
+			return []byte(config.Conf.SecretKeyJWT), nil
+		})
 
-// 		if err != nil { //Malformed token, returns with http code 403 as usual
-// 			log.Impl.Error("Неверно сформированный токен аутентификации")
-// 			c.AbortWithStatusJSON(403, gin.H{"error": "Неверно сформированный токен аутентификации"})
-// 			return
-// 		}
+		if err != nil { //Malformed token, returns with http code 403 as usual
+			log.Impl.Error("Неверно сформированный токен аутентификации")
+			c.AbortWithStatusJSON(403, gin.H{"error": "Неверно сформированный токен аутентификации"})
+			return
+		}
 
-// 		if !token.Valid { //Token is invalid, maybe not signed on this server
-// 			log.Impl.Error("Токен недействителен")
-// 			c.AbortWithStatusJSON(403, gin.H{"error": "Токен недействителен"})
-// 			return
-// 		}
+		if !token.Valid { //Token is invalid, maybe not signed on this server
+			log.Impl.Error("Токен недействителен")
+			c.AbortWithStatusJSON(403, gin.H{"error": "Токен недействителен"})
+			return
+		}
 
-// 		//str := fmt.Sprint(claims.UserId) //Useful for monitoring
-// 		//fmt.Println(str)
-// 		// ctx := context.WithValue(r.Context(), "user", tk.UserId)
+		//str := fmt.Sprint(claims.UserId) //Useful for monitoring
+		//fmt.Println(str)
+		// ctx := context.WithValue(r.Context(), "user", tk.UserId)
 
-// 		c.Next()
-// 	}
-// }
+		c.Next()
+	}
+}
 
 //a struct to rep user account
 type Account struct {
