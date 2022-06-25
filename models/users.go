1835ef73d5812b2abb09ed7b2ca89b1f24280196
dmitry-msk777 <dima-irk35@mail.ru>
2022-06-26 01:34:17 +0300

    new

--- models/users.go
+++ models/users.go
@@ -4,14 +4,15 @@ import "gorm.io/gorm"
 
 type LkUsers struct {
 	gorm.Model
-	FullName        string
-	UserID          string
-	Login           string `json:"login"`
-	Password        string `json:"Password"`
-	HashPassword    string
-	SecretJWT       string `gorm:"index:idx_lk_users_jw_ttoken,type:btree"`
-	JWTtoken        string
-	JWTExp          int64
+	FullName     string
+	UserID       string
+	Login        string `json:"login"`
+	Password     string `json:"Password"`
+	HashPassword string
+	SecretJWT    string //`gorm:"index:idx_lk_users_jw_ttoken,type:btree"`
+	//JWTtoken        string
+	//JWTExp          int64
+	ExpSec          int64
 	Role            string
 	InsuranceNumber string
 	Email           string
