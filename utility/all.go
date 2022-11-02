bb71c9d8c23f3cce2e88fe973c13f65d70ffb2e6
dmitry-msk777 <dima-irk35@mail.ru>
2022-11-02 13:21:34 +0300

    new

--- utility/all.go
+++ utility/all.go
@@ -301,7 +301,8 @@ func GetAllDataFromTables(DB *sql.DB, TableNameParam string, mapAvailableTables 
 		if PaginatioRegim {
 			queryBuilder = psql.Select("*").From(TableNameParam).Where(sq.GtOrEq{"updated_at": timePast}).Limit(uint64(Limit)).Offset(uint64(Offset)).OrderBy("created_at")
 		} else {
-			queryBuilder = psql.Select("*").From(TableNameParam).Where(sq.GtOrEq{"updated_at": timePast})
+			//queryBuilder = psql.Select("*").From(TableNameParam).Where(sq.GtOrEq{"updated_at": timePast})
+			queryBuilder = psql.Select("*").From(TableNameParam).Where(sq.Or{sq.GtOrEq{"updated_at": timePast}, sq.GtOrEq{"deleted_at": timePast}, sq.GtOrEq{"created_at": timePast}})
 		}
 		param = append(param, timePast)
 
