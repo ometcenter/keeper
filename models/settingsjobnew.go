25e63c9eebb09f9ff400f6c009ad8d43228c739f
dmitry-msk777 <dima-irk35@mail.ru>
2023-02-07 23:15:54 +0300

    new

--- models/settingsjobnew.go
+++ models/settingsjobnew.go
@@ -86,7 +86,7 @@ func (S SettingsJobsAllV2) SaveSettingsToPg(DB *sql.DB) error {
 	// }
 
 	var counter int
-	DB.QueryRow("SELECT count(*) FROM settings_jobs777 WHERE job_id = $1", argsquery...).Scan(&counter)
+	DB.QueryRow("SELECT count(*) FROM settings_jobs_v2 WHERE job_id = $1", argsquery...).Scan(&counter)
 
 	if counter != 0 {
 		//if flag == true {
@@ -100,7 +100,7 @@ func (S SettingsJobsAllV2) SaveSettingsToPg(DB *sql.DB) error {
 
 		// _, err := store.DB.Exec(`UPDATE settings_jobs SET job_id = $1, json_string = $2, code_external = $3,
 		// name_external = $4, table_name = $5, use_remote_collection = $6, config_name = $7 WHERE job_id = $1;`, argsUpdate...)
-		_, err := DB.Exec(`UPDATE settings_jobs777 SET job_id = $1, code_external = $2, 
+		_, err := DB.Exec(`UPDATE settings_jobs_v2 SET job_id = $1, code_external = $2, 
 		 name_external = $3, table_name = $4, json_byte = $5 WHERE job_id = $1;`, argsUpdate...)
 
 		if err != nil {
@@ -125,7 +125,7 @@ func (S SettingsJobsAllV2) SaveSettingsToPg(DB *sql.DB) error {
 		// _, err := store.DB.Exec(`INSERT INTO settings_jobs (job_id, json_string, code_external, name_external, table_name, use_remote_collection, config_name)
 		// VALUES($1, $2, $3, $4, $5, $6, $7);`, argsInsert...)
 
-		_, err := DB.Exec(`INSERT INTO settings_jobs777 (job_id, code_external, name_external, table_name, json_byte)
+		_, err := DB.Exec(`INSERT INTO settings_jobs_v2 (job_id, code_external, name_external, table_name, json_byte)
 		VALUES($1, $2, $3, $4, $5);`, argsInsert...)
 
 		if err != nil {
