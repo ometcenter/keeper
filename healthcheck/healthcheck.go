4d5f2d0c13534e58802aa8544e1bdf9e094e5116
dmitry-msk777 <dima-irk35@mail.ru>
2023-02-22 12:06:25 +0300

    new

--- healthcheck/healthcheck.go
+++ healthcheck/healthcheck.go
@@ -1,6 +1,7 @@
 package healthcheck
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 
@@ -38,7 +39,7 @@ func StartHealthCheckLight() {
 	health := healthcheck.NewHandler()
 
 	// Our app is not happy if we've got more than 100 goroutines running.
-	health.AddLivenessCheck("goroutine-threshold", healthcheck.GoroutineCountCheck(300))
+	health.AddLivenessCheck("goroutine-threshold", healthcheck.GoroutineCountCheck(100))
 
 	// // Our app is not ready if we can't resolve our upstream dependency in DNS.
 	// health.AddReadinessCheck(
@@ -78,5 +79,62 @@ func StartHealthCheckTestError() {
 	health.AddReadinessCheck("database", healthcheck.DatabasePingCheck(DB, 5*time.Second))
 
 	go http.ListenAndServe("0.0.0.0:8086", health)
+
+}
+
+func HealthCheckTestGoroutines() {
+
+	// vars := mux.Vars(r)
+	// JobId := vars["id"]
+
+	// err := store.CancelJob(JobId)
+
+	// if err != nil {
+	// 	log.Impl.Error(err)
+	// 	fmt.Fprintf(w, err.Error())
+	// 	return
+	// }
+
+	jobs := make(chan int, 5)
+	done := make(chan bool)
+
+	go func() {
+		for {
+			j, closed := <-jobs
+			if closed {
+				fmt.Println("received job", j)
+			} else {
+				fmt.Println("received all jobs")
+				done <- true
+				return
+			}
+		}
+	}()
+
+	for i := 0; i < 320; i++ {
+		go func(counter int) {
+			for j := 0; j < 10; j++ {
+
+				// select {
+				// case msg1 := <-done:
+				// 	fmt.Println("received done", msg1)
+				// 	if msg1 {
+				// 		return
+				// 	}
+				// default:
+				jobs <- counter
+				time.Sleep(1 * time.Second)
+				// }
+
+			}
+		}(i)
+	}
+
+	go func() {
+		time.Sleep(20 * time.Second)
+		fmt.Println("sent all jobs")
+		done <- true
+		close(jobs)
+	}()
 
 }
